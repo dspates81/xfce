ecb1af31231f1f5235cd989879a72d93814779df
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-07-29 15:00:30 -0400

    Update Aur.sh

--- Aur.sh
+++ Aur.sh
@@ -3,8 +3,8 @@
 mkdir Aur_file
 cd Aur_file
 
-https://aur.archlinux.org/brave.git
-https://aur.archlinux.org/vmware-workstation.git
-https://aur.archlinux.org/vmware-keymaps.git
-https://aur.archlinux.org/tiny-media-manager.git
-https://aur.archlinux.org/google-chrome.git
+git clone https://aur.archlinux.org/brave.git
+git clone https://aur.archlinux.org/vmware-workstation.git
+git clone https://aur.archlinux.org/vmware-keymaps.git
+git clone https://aur.archlinux.org/tiny-media-manager.git
+git clone https://aur.archlinux.org/google-chrome.git
