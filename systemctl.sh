70329eeea6cc943dc2819d8673b7b447d75f22f2
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-07 22:11:41 -0400

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+systemctl enable smb.service
 systemctl enable lightdm.service
 systemctl enable bluetooth
 systemctl enable sshd
-systemctl enable avahi-daemon
+#systemctl enable avahi-daemon
 systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
-systemctl enable firewalld
+#systemctl enable firewalld
 systemctl enable acpid
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
