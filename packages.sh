a0f9f7ef7fa6bc7a1f41a39a815c2b08e7419305
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-07 22:09:23 -0400

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+
+
+
+
+sudo pacman -S --needed arc-gtk-theme arc-icon-theme arch-install-scripts archiso archivetools aspell-en autoconf automake bind binutils bison bridge-utils code cronie dialog exo firefox flex garcon gcc gedit gnome-disk-utility gparted handbrake htop kde-cli-tools krusader libdvdcss libisoburn lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings man-db pacman-contrib patch pavucontrol pcsclite pkgconf pulseaudio pulseaudio-bluetooth pulseaudio-jack python2 qbittorrent squashfs-tools terminator terminus-font texinfo transcode tumbler vde2 virt-manager vlc k3b qemu qemu-arch-extra terminus-font dolphin dolphin-plugins
+
+
+
+
+
+
+
