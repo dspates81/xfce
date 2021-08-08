fd532edee713e6626ca28306100a3361cfa92ac5
dspates81 <72100421+dspates81@users.noreply.github.com>
2021-08-08 12:14:47 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-
-sudo pacman -S exo garcon xfce4-appfinder xfce4-panel xfce4-power-manager xfce4-session xfce4-settings xfconf xfdesktop xfwm4 xfwm4-themes mousepad ristretto xfburn xfce4-artwork xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-dict xfce4-diskperf-plugin xfce4-eyes-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mailwatch-plugin xfce4-mount-plugin xfce4-mpc-plugin xfce4-netload-plugin xfce4-notes-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-sensors-plugin xfce4-smartbookmark-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-time-out-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-weather-plugin xfce4-xkb-plugin parole xfce4-whiskermenu-plugin xorg xorg-server
+sudo pacman -Syu
 
 
+sudo pacman -S --needed arc-gtk-theme arc-icon-theme arch-install-scripts archiso archivetools aspell-en autoconf automake bind binutils bison bridge-utils code cronie dialog exo firefox flex garcon gcc gedit gnome-disk-utility gparted handbrake htop kde-cli-tools krusader libdvdcss libisoburn lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings man-db pacman-contrib patch pavucontrol pcsclite pkgconf pulseaudio pulseaudio-bluetooth pulseaudio-jack python2 qbittorrent squashfs-tools terminator terminus-font texinfo transcode tumbler vde2 virt-manager vlc k3b qemu qemu-arch-extra terminus-font dolphin dolphin-plugins lxde-icon-theme mate-icon-theme-faenza awesome-terminal-fonts breeze-icons libqtxdg libicns kiconthemes kemoticons gtk-update-icon-cache
 
-sudo ./xfce/packages.sh
-sudo ./xfce/systemctl.sh
